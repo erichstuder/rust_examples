393d7a55e21d93bbb3bc820495de54b6dc1a63e1
erichstuder <richu_@hotmail.com>
2025-04-21 15:52:13 +0200

    some cleanup

--- software/src/main.rs
+++ software/src/main.rs
@@ -2,15 +2,24 @@
 //!
 //! To run the program type `cargo run` in the terminal.
 
-fn main() {
-    let mut text: String = String::from("Hello, world!");
-    my_print_mut_ref(&mut text);
-    my_print_mut_ref(&mut text);
+mod tasks;
 
-    //run_tasks();
+fn main() {
+    println!("\n***rust examples***\n");
+
+    let /*mut*/ text: String = String::from("Hello, world!");
+    my_print(text);
+    //my_print(text);
+
+    //my_print_ref(&text);
+    //my_print_ref(&text);
+
+    //my_print_mut_ref(&mut text);
+    //my_print_mut_ref(&mut text);
+
+    //tasks::run();
 }
 
-// This is a function.
 // Note: No forward declaration is needed like for example in C.
 // #[allow(unused)] suppresses the warning for unused function. Normally, this is not needed.
 #[allow(unused)]
@@ -24,6 +33,7 @@ fn my_print_ref(text: &String) {
     println!("{}", text);
 }
 
+#[allow(unused)]
 fn my_print_mut_ref(text: &mut String) {
     text.push_str("\n");
     println!("{}", text);
